675b9be26fa13841e7baeabba52f23bc64cf5085
John Chodera <john.chodera@choderalab.org>
2018-08-07 13:45:39 -0400

    Run serially and delete waters from peptide.pdb

--- buck/1klu/run-lsf-mhc.sh
+++ buck/1klu/run-lsf-mhc.sh
@@ -10,12 +10,14 @@
 #BSUB -q gpuqueue
 #
 # nodes: number of nodes and GPU request
-#BSUB -n 12
+#BSUB -n 1
 #BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
 ##BSUB -R "{rusage[mem=12,ngpus_physical=2] span[ptile=2]} || {rusage[mem=12,ngpus_physical=3] span[ptile=3]} || {rusage[mem=12,ngpus_physical=4] span[ptile=4]} || {rusage[mem=12,ngpus_physical=1] span[ptile=1]}"
 #
 # job name (default = name of script file)
 #BSUB -J "mhc"
 
-build_mpirun_configfile --hostfilepath hostfile.${LSB_JOBNAME} --configfilepath configfile.${LSB_JOBNAME} "python run_yank.py --yaml=${LSB_JOBNAME}.yaml"
-mpiexec.hydra -f hostfile.${LSB_JOBNAME} -configfile configfile.${LSB_JOBNAME}
+#build_mpirun_configfile --hostfilepath hostfile.${LSB_JOBNAME} --configfilepath configfile.${LSB_JOBNAME} "python run_yank.py --yaml=${LSB_JOBNAME}.yaml"
+#mpiexec.hydra -f hostfile.${LSB_JOBNAME} -configfile configfile.${LSB_JOBNAME}
+
+python run_yank.py --yaml=${LSB_JOBNAME}.yaml
