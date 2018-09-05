1c12a17ec16b2e3c36fcbbca9558434de38b8e0f
Kevin Boehm <kevinboehm@lumino.mskcc.org>
2018-09-05 15:38:52 -0400

    first attempt at checkStability script for short OpenMM simulation

---
+++ checkStability.py
@@ -0,0 +1,16 @@
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from sys import stdout
+
+pdb = PDBFile('1klu-pdbfixer-nosolvent.pdb')
+with open('system.xml', 'rb') as f:
+   system = XmlSerializer.deserialize(f.read())
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+simulation = Simulation(pdb.topology, system, integrator)
+simulation.context.setPositions(pdb.positions)
+simulation.minimizeEnergy()
+simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+        potentialEnergy=True, temperature=True))
+simulation.step(10000)
