35087938eac351c21a1370a9700da458cc97ec2d
Kevin Boehm <boehmk@lt11.hpc.private>
2018-09-10 15:02:06 -0400

    including complex xml files

--- checkStability.py
+++ checkStability.py
@@ -3,14 +3,14 @@ from simtk.openmm import *
 from simtk.unit import *
 from sys import stdout
 
-pdb = PDBFile('1klu-minimized.pdb')
-with open('system.xml', 'r') as f:
+pdb = PDBFile('1kluComplex-minimized.pdb')
+with open('1kluComplexSystem.xml', 'r') as f:
    system = XmlSerializer.deserialize(f.read())
 integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
 simulation = Simulation(pdb.topology, system, integrator)
 simulation.context.setPositions(pdb.positions)
 simulation.minimizeEnergy()
-simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(PDBReporter('Complexoutput.pdb', 1000))
 simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
         potentialEnergy=True, temperature=True))
 simulation.step(10000)
