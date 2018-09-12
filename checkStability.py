944249f9a01f501028d67feae4416acb097647da
Kevin Boehm <kevinboehm@lumino.mskcc.org>
2018-09-12 14:55:35 -0400

    functionalized code

--- checkStability.py
+++ checkStability.py
@@ -3,14 +3,24 @@ from simtk.openmm import *
 from simtk.unit import *
 from sys import stdout
 
-pdb = PDBFile('1kluLigand-minimized.pdb')
-with open('1kluLigandSystem.xml', 'r') as f:
-   system = XmlSerializer.deserialize(f.read())
-integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
-simulation = Simulation(pdb.topology, system, integrator)
-simulation.context.setPositions(pdb.positions)
-simulation.minimizeEnergy()
-simulation.reporters.append(PDBReporter('Complexoutput.pdb', 1000))
-simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
-        potentialEnergy=True, temperature=True))
-simulation.step(10000)
+def check_stability(pdbFn, xmlFn):
+    pdb = PDBFile(pdbFn)
+    with open(xmlFn, 'r') as f:
+       system = XmlSerializer.deserialize(f.read())
+    integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+    simulation = Simulation(pdb.topology, system, integrator)
+    simulation.context.setPositions(pdb.positions)
+    simulation.minimizeEnergy()
+    simulation.reporters.append(PDBReporter('Complexoutput.pdb', 1000))
+    simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+            potentialEnergy=True, temperature=True))
+    simulation.step(10000)
+
+if __name__ == '__main__':
+    from sys import argv
+    from subroutines import get_opts
+
+    opts = get_opts(argv)
+    pdbFn = opts['-pdbfn']
+    xmlFn = opts['-xmlfn']
+    check_stability(pdbFn, xmlFn)
--- prepPdbFile.py
+++ prepPdbFile.py
@@ -4,124 +4,121 @@
 Retrieve a PDB file from the RCSB, solvate it, and minimize.
 
 """
-
 ################################################################################
 # IMPORTS
 ################################################################################
-
 from simtk import unit
 from simtk import openmm
 from simtk.openmm import app
 from pdbfixer import PDBFixer
+from subroutines import write_file
 
 ################################################################################
-# OPTIONS
+# MAIN FUNCTION
 ################################################################################
+def prep_pdb_file(pdbid, chain_ids_to_keep, fnId):
+    '''
+    :param pdbid: PDB ID to retrieve, e.g. '1klu'
+    :param chain_ids_to_keep: chains to keep, e.g. ['A', 'B', 'C']
+    :param fnId: filename prefix, e.g. 'complex'
+    :return: null. Saves pdb files, serialized xml system, and integrator
+    '''
+    fnPrefix = pdbid + '-' + fnId + '-'
+    pH = 7.0 # pH
+    forcefields_to_use = ['amber99sbildn.xml', 'tip3p.xml'] # list of forcefields to use in parameterization
+    padding = 11.0 * unit.angstroms # padding to use for adding solvent
+    nonbondedMethod = app.PME # nonbonded method
+    constraints = app.HBonds # bonds to be constrained
+    keepWater = False # keep crystal water
 
-pdbid = '1klu'  # PDB ID to retrieve
-chain_ids_to_keep = ['C'] #['A', 'B', 'C']  # chains to keep
-if len(chain_ids_to_keep) > 1:
-    fnPrefix = pdbid + 'Complex'
-else:
-    fnPrefix = pdbid + 'Ligand'
-pH = 7.0 # pH
-forcefields_to_use = ['amber99sbildn.xml', 'tip3p.xml'] # list of forcefields to use in parameterization
-padding = 11.0 * unit.angstroms # padding to use for adding solvent
-nonbondedMethod = app.PME # nonbonded method
-constraints = app.HBonds # bonds to be constrained
-keepWater = False # keep crystal water
+    # Load forcefield.
+    forcefield = app.ForceField(*forcefields_to_use)
 
-################################################################################
-# SUBROUTINES
-################################################################################
+    # Retrieve structure from PDB.
+    print('Retrieving %s from PDB...' % pdbid)
+    fixer = PDBFixer(pdbid=pdbid)
 
-def write_file(filename, contents):
-    outfile = open(filename, 'w')
-    outfile.write(contents)
-    outfile.close()
+    # Build a list of chains to remove.
+    print('Removing all chains but %s' % chain_ids_to_keep)
+    all_chains = list(fixer.topology.chains())
+    chain_id_list = [c.id for c in fixer.topology.chains()]
+    chain_ids_to_remove = set(chain_id_list) - set(chain_ids_to_keep)
+    fixer.removeChains(chainIds=chain_ids_to_remove)
 
-################################################################################
-# SET UP SYSTEM
-################################################################################
+    # Find missing residues.
+    print('Finding missing residues...')
+    fixer.findMissingResidues()
 
-# Load forcefield.
-forcefield = app.ForceField(*forcefields_to_use)
+    # Replace nonstandard residues.
+    print('Replacing nonstandard residues...')
+    fixer.findNonstandardResidues()
+    fixer.replaceNonstandardResidues()
 
-# Retrieve structure from PDB.
-print('Retrieving %s from PDB...' % pdbid)
-fixer = PDBFixer(pdbid=pdbid)
+    # Add missing atoms.
+    print('Adding missing atoms...')
+    fixer.findMissingAtoms()
+    fixer.addMissingAtoms()
 
-# Build a list of chains to remove.
-print('Removing all chains but %s' % chain_ids_to_keep)
-all_chains = list(fixer.topology.chains())
-chain_id_list = [c.id for c in fixer.topology.chains()]
-chain_ids_to_remove = set(chain_id_list) - set(chain_ids_to_keep)
-fixer.removeChains(chainIds=chain_ids_to_remove)
+    # Remove heterogens.
+    print('Removing heterogens...')
+    fixer.removeHeterogens(keepWater=keepWater)
 
-# Find missing residues.
-print('Finding missing residues...')
-fixer.findMissingResidues()
+    # Add missing hydrogens.
+    print('Adding missing hydrogens appropriate for pH %s' % pH)
+    fixer.addMissingHydrogens(pH)
 
-# Replace nonstandard residues.
-print('Replacing nonstandard residues...')
-fixer.findNonstandardResidues()
-fixer.replaceNonstandardResidues()
+    # Write PDB file.
+    output_filename = '%s-pdbfixer-nosolvent.pdb' % fnPrefix
+    print('Writing PDB file to "%s"...' % output_filename)
+    app.PDBFile.writeFile(fixer.topology, fixer.positions, open(output_filename, 'w'), keepIds=True)
 
-# Add missing atoms.
-print('Adding missing atoms...')
-fixer.findMissingAtoms()
-fixer.addMissingAtoms()
+    if nonbondedMethod in [app.PME, app.CutoffPeriodic, app.Ewald]:
+        # Add solvent.
+        print('Adding solvent...')
+        fixer.addSolvent(padding=padding, positiveIon='Na+', negativeIon='Cl-', ionicStrength=200e-3*unit.molar)
 
-# Remove heterogens.
-print('Removing heterogens...')
-fixer.removeHeterogens(keepWater=keepWater)
+    # Write PDB file.
+    output_filename = '%s-pdbfixer.pdb' % fnPrefix
+    print('Writing PDB file to "%s"...' % output_filename)
+    app.PDBFile.writeFile(fixer.topology, fixer.positions, open(output_filename, 'w'), keepIds=True)
 
-# Add missing hydrogens.
-print('Adding missing hydrogens appropriate for pH %s' % pH)
-fixer.addMissingHydrogens(pH)
+    # Create OpenMM System.
+    print('Creating OpenMM system...')
+    system = forcefield.createSystem(fixer.topology, nonbondedMethod=nonbondedMethod,
+                                     constraints=constraints, rigidWater=True, removeCMMotion=False)
 
-# Write PDB file.
-output_filename = '%s-pdbfixer-nosolvent.pdb' % fnPrefix
-print('Writing PDB file to "%s"...' % output_filename)
-app.PDBFile.writeFile(fixer.topology, fixer.positions, open(output_filename, 'w'))
+    # Minimimze to update positions.
+    print('Minimizing...')
+    integrator = openmm.VerletIntegrator(1.0 * unit.femtosecond)
+    context = openmm.Context(system, integrator)
+    context.setPositions(fixer.positions)
+    openmm.LocalEnergyMinimizer.minimize(context)
+    state = context.getState(getPositions=True)
+    fixer.positions = state.getPositions()
 
-if nonbondedMethod in [app.PME, app.CutoffPeriodic, app.Ewald]:
-    # Add solvent.
-    print('Adding solvent...')
-    fixer.addSolvent(padding=padding, positiveIon='Na+', negativeIon='Cl-', ionicStrength=200e-3*unit.molar)
+    # Write final coordinates.
+    output_filename = '%s-minimized.pdb' % fnPrefix
+    print('Writing PDB file to "%s"...' % output_filename)
+    app.PDBFile.writeFile(fixer.topology, fixer.positions, open(output_filename, 'w'), keepIds=True)
 
-# Write PDB file.
-output_filename = '%s-pdbfixer.pdb' % fnPrefix
-print('Writing PDB file to "%s"...' % output_filename)
-app.PDBFile.writeFile(fixer.topology, fixer.positions, open(output_filename, 'w'))
+    # Serialize final coordinates.
+    print('Serializing to XML...')
+    system_filename = fnPrefix + 'system.xml'
+    integrator_filename = fnPrefix + 'integrator.xml'
+    state_filename = fnPrefix + 'state.xml'
+    write_file(system_filename, openmm.XmlSerializer.serialize(system))
+    write_file(integrator_filename, openmm.XmlSerializer.serialize(integrator))
+    state = context.getState(getPositions=True, getVelocities=True, getForces=True, getEnergy=True,
+                             getParameters=True, enforcePeriodicBox=True)
+    write_file(state_filename, openmm.XmlSerializer.serialize(state))
 
-# Create OpenMM System.
-print('Creating OpenMM system...')
-system = forcefield.createSystem(fixer.topology, nonbondedMethod=nonbondedMethod,
-                                 constraints=constraints, rigidWater=True, removeCMMotion=False)
 
-# Minimimze to update positions.
-print('Minimizing...')
-integrator = openmm.VerletIntegrator(1.0 * unit.femtosecond)
-context = openmm.Context(system, integrator)
-context.setPositions(fixer.positions)
-openmm.LocalEnergyMinimizer.minimize(context)
-state = context.getState(getPositions=True)
-fixer.positions = state.getPositions()
-
-# Write final coordinates.
-output_filename = '%s-minimized.pdb' % fnPrefix
-print('Writing PDB file to "%s"...' % output_filename)
-app.PDBFile.writeFile(fixer.topology, fixer.positions, open(output_filename, 'w'))
-
-# Serialize final coordinates.
-print('Serializing to XML...')
-system_filename = fnPrefix + 'System.xml'
-integrator_filename = fnPrefix + 'Integrator.xml'
-state_filename = fnPrefix + 'State.xml'
-write_file(system_filename, openmm.XmlSerializer.serialize(system))
-write_file(integrator_filename, openmm.XmlSerializer.serialize(integrator))
-state = context.getState(getPositions=True, getVelocities=True, getForces=True, getEnergy=True,
-                         getParameters=True, enforcePeriodicBox=True)
-write_file(state_filename, openmm.XmlSerializer.serialize(state))
-
+if __name__ == '__main__':
+    from subroutines import get_opts
+    from sys import argv
+    opts = get_opts(argv)
+    pdbid = opts['-pdbid']
+    chain_string = opts['-chains']
+    chain_ids_to_keep = chain_string.split(',').replace(' ', '')
+    fnId = opts['-id']
+    prep_pdb_file(pdbid, chain_ids_to_keep, fnId)
